66257237e4c85b9fd77f70a814a49f14e4f49d0c
Gustavo D. Muniz <gustavo_dmuniz@yahoo.com.br>
2020-11-01 21:41:00 -0300

    Clear code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,8 +20,6 @@ require("packs/loan_applicant/form")
 // SIMULATE_LOAN
 require("packs/simulate_loan/index")
 
-
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
